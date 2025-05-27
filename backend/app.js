df5e0ecebb9ba80d58d4a99d420e58e98952eac4
psa-tech-2025 <services.psatech@gmail.com>
2025-05-27 09:03:05 +0530

    "backend-changes added"

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connected = require('./config/db');
+
+dotenv.config();
+connected();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth.routes'));
+
+const PORT  = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`sertver connected  ${PORT}`));
---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+const User = require('../models/user.model');
+const bcrypt =require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async( req , res ) => {
+      const { name, mobilenumber, email, password } = req.body;
+
+      try {
+        const existing = await User.findOne({ email });
+         if (existing) return res.status(400).json({ message: 'Email already registered' });
+            const hashedPassword = await bcrypt.hash(password, 10);
+        //  const ismatch = await bcrypt.compare(password, User.password);
+        //  if(!ismatch) return res.status(400).json({ message :'invalid emai and password'})
+         const user = new User({ name, mobilenumber, email, password: hashedPassword });
+            await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+
+      } catch (err) {
+        res.status(500).json({ message: 'Registration failed', error: err.message });
+      }
+}
+
+exports.login =async(req,res) => {
+ const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token, user: { name: user.name, email: user.email, mobilenumber: user.mobilenumber } });
+  } catch (err) {
+    res.status(500).json({ message: 'Login failed', error: err.message });
+  }
+};
+
+
